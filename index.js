69e017ebdb1ef6fe13541d7407fa52b2b591f5c8
achi <achiiinasaridze@gmail.com>
2023-03-26 23:25:07 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+/* First Homework - Youtube */
+
+// let userIsLoggedIn = true;
+// let videoIsCommentable = true;
+
+// const userCanComment = userIsLoggedIn && videoIsCommentable;
+
+// console.log(userCanComment);
+
+
+
+/* Second Homework - Social media */
+
+// let userIsLoggedIn = true;
+// let userIsBlocked = false;
+
+// const userCanPost = userIsLoggedIn && !userIsBlocked;
+
+// console.log(userCanPost);
+
+
+
+/* Third Homework - Online Shop */
+
+// let balance = 2;
+// let userCartTotal = 4;
+
+// const paymentComplite = balance >= userCartTotal;
+
+// console.log(paymentComplite);
+
+
+/* Forth Homework - Delete coment */
+
+// let userIsAdmin = false;
+// let userIsModerator = false;
+// let userIsComentAuthor = false;
+
+// const userCanDelete = userIsAdmin || userIsModerator || userIsComentAuthor;
+
+// console.log(userCanDelete);
+
+
+
+/* Fifth Homework - Atomic bomb */
+
+let presidentKey = true;
+let primeMinisterKey = false;
+let generalOfArmiesKey = true;
+let masterKey = true;
+
+const bombWillLaunch = (presidentKey && primeMinisterKey && generalOfArmiesKey) || masterKey;
+
+console.log(bombWillLaunch);
