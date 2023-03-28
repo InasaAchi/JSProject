4af83b2c5e761ba732485f1b1faa8b16a84b3adb
achi <achiiinasaridze@gmail.com>
2023-03-28 16:18:38 +0400

    Pin code

--- index.js
+++ index.js
@@ -44,11 +44,40 @@
 
 /* Fifth Homework - Atomic bomb */
 
-let presidentKey = true;
-let primeMinisterKey = false;
-let generalOfArmiesKey = true;
-let masterKey = false;
+// let presidentKey = true;
+// let primeMinisterKey = false;
+// let generalOfArmiesKey = true;
+// let masterKey = false;
 
-const bombWillLaunch = (presidentKey && primeMinisterKey && generalOfArmiesKey) || masterKey;
+// const bombWillLaunch = (presidentKey && primeMinisterKey && generalOfArmiesKey) || masterKey;
 
-console.log(bombWillLaunch);
+// console.log(bombWillLaunch);
+
+
+
+/* Pin code */
+
+const userPinCode = 7825;
+
+const currentPinCode = 7825;
+const newPinCode = 2825;
+const retypeNewPinCode = 2825;
+
+const pinChangeComplete = ((userPinCode == currentPinCode) && (newPinCode == retypeNewPinCode));
+
+console.log("Please enter pin code");
+
+if(userPinCode == currentPinCode) {
+    console.log("It's been a long time before you have changed pin Code - Please enter new Pin code");
+    console.log("Retype new pin")
+    if(newPinCode !== retypeNewPinCode) {
+        console.log("New Pin code is incorrect - please enter the correct pin");
+    } else {
+        console.log("Pin has Changed")
+    }
+} else {
+    console.log("Pin is incorect - please enter correct pin");
+}
+
+
+console.log(pinChangeComplete);
