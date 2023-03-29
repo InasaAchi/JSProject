45b301886d52c0e5643cba75774eab4bdf1b389a
achi <achiiinasaridze@gmail.com>
2023-03-29 14:26:30 +0400

    Update 29.03

--- index.js
+++ index.js
@@ -82,12 +82,56 @@
 
 // console.log(pinChangeComplete);
 
+
+
 /* GAME OVER */
 
-let health = 1;
-let lives = 0;
+// let health = 1;
+// let lives = 0;
 
-const gameOver = (health == 0 && lives == 0) || (lives < 3 == 0 && health);
+// const gameOver = (health == 0 && lives == 0) || (lives < 3 == 0 && health);
 
-console.log(gameOver);
+// console.log(gameOver);
 
+
+
+/* MEAMA */
+
+// let capsuleCount = 0;
+// let capsulePrice = 2;
+// let userBalance = 0;
+// let creditCardStatus = userBalance >= capsulePrice || userBalance <= capsulePrice;
+
+// if(userBalance < capsulePrice) {
+//     console.log("There isn't enouhg money in your account");
+// }
+
+// if(capsuleCount < 1) {
+//     console.log("I'm sorry, there is not enougn capsule");
+// }
+
+// const paymentComplete = userBalance >= capsulePrice && capsuleCount > 0;
+
+// console.log(paymentComplete);
+
+
+
+/* Hotel Booking */
+
+let availableRooms = 10;
+let roomPrice = 150;
+let discount = (30/150)*100;
+
+let userBalance = 1500;
+let userHasDiscount = false;
+let userRoomCount = 3;
+
+const paymentComplete = userBalance >= roomPrice && availableRooms >= userRoomCount && (userRoomCount * roomPrice) <= userBalance;
+
+if(userHasDiscount) {
+    console.log("You have discount, 20% of the total amount will be reduced");
+} else {
+    console.log("Unfortunately, you don't have a discount");
+}
+
+console.log(paymentComplete);
